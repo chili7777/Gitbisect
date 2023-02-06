f0f72cfb1f67b21f1458e435106ce37c8b41e4f1
Hanmilton Berrezueta <hberrezu@pichincha.com>
2023-02-06 17:06:29 -0500

    third feature

--- src/GitBisectCapacitacion.java
+++ src/GitBisectCapacitacion.java
@@ -4,5 +4,7 @@ public class GitBisectCapacitacion
     {
         System.out.println("Feature 1");
         System.out.println("Feature 2");
+        System.out.println("Feature 3 BUG"L);KAJSDF
+
     }
 }
