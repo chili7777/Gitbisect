6b6c339c77e64d98bee258cb7c001f8b6f721212
Hanmilton Berrezueta <hberrezu@pichincha.com>
2023-02-06 17:43:14 -0500

    fasdfasd

--- src/GitBisectCapacitacion.java
+++ src/GitBisectCapacitacion.java
@@ -6,7 +6,7 @@ public class GitBisectCapacitacion
         System.out.println("Feature 2");
         System.out.println("Feature 3");
         System.out.println("Feature 4");
-        System.out.println("Feature 5"sdflasjdf);f;laksd;fk
+        System.out.println("Feature 5");
 
     }
 }
